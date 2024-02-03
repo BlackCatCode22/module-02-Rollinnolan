9bd26ec35ada748ccd10c430e6e38e85de165cef
Rollinnolan <109935701+Rollinnolan@users.noreply.github.com>
2024-02-02 17:02:28 -0800

    Add files via upload

--- ReversedStrings.java
+++ ReversedStrings.java
@@ -1,30 +1,54 @@
 import java.util.Scanner;
+public class ReversedStrings
+{
+    public static void main(String[] args)
+    {
+        Scanner stdin = new Scanner(System.in);
+        System.out.println("Please enter a word or sentence.");
+        String input = stdin.nextLine();
+        String word = "";
+        String output = "";
+        /*====================================================================
+        ======================================================================*/
+        /*This is my first attempt before the extension*/
+        /*for(int i = input.length(); i > 0; i--)
+        {
+            //length is 1 number higher than the last index char
+            char c = input.charAt(i - 1);
+            output = output + c;
+        }*/
+        /*====================================================================
+        ======================================================================*/
+        /*This for loop iterates after each word*/
+        for(int i = 1; i <= input.length(); i = i + word.length())
+        {
+            word = input.substring(i-1);
+            if (word.substring(1).contains(" "))
+            {
+                word = word.trim(); // trim a space that comes at the end of the index
+                word = word.substring(0,word.indexOf(' '));
+                for(int j = word.length(); j > 0; j--)
+                {
+                    //length is 1 number higher than the last index char
+                    char c = word.charAt(j - 1);
+                    output = output + c;
+                }
+                output = output + ' ';
+                word = word + ' ';
+                /*add the space back so that word.length() has the correct value*/
+            }
+            //This part works when there are no spaces at the end
+            else
+            {
+                for(int j = word.length(); j > 0; j--)
+                {
+                    //length is 1 number higher than the last index char
+                    char c = word.charAt(j - 1);
+                    output = output + c;
+                }
+            }
 
-public class ReversedStrings {
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to My Strings program");
-
-        // Create a Scanner object to get a string from the user.
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\n Please enter a string of characters...\n");
-        String userInput = scanner.nextLine();
-
-        // Output the line to reverse.
-        System.out.println("\n\nThe string to reverse is: " + userInput + "\n\n");
-
-        // Strings are a data structure meaning we can access individual characters like this
-        char aCharFromAString = userInput.charAt(3);
-
-        System.out.println("\n\n aCharFromAString is " + aCharFromAString);
-
-        String reversedString = "";
-        for (int i = 0; i < userInput.length(); i++) {
-            System.out.println("\n  " + userInput.charAt(i) );
-            reversedString = userInput.charAt(i) + reversedString;
         }
-
-        System.out.println("\n reversed Str = " + reversedString);
-
+        System.out.println("Here is your response backwards:  "+output);
     }
 }
